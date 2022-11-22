a58c299b46763a29fc9b3a31d915ee5b092e029f
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2022-11-21 18:32:39 -0600

    user class and usertest

--- app/models/User.js
+++ app/models/User.js
@@ -7,17 +7,40 @@ class User {
         this.height = height;
         this.weight = weight;
         this.instalment = instalment;
+        this.dateEnrollment = new Date();
+        this.currentDate = new Date();
     }
 
+    //getters
     getName() { return this.name }
 
     getLastName() { return this.lastName; }
 
-    getAge(){return this.age;}
+    getAge() { return this.age; }
 
-    getHeight(){return this.height;}
+    getHeight() { return this.height; }
 
-    getWeight(){return this.weight;}
+    getWeight() { return this.weight; }
 
-    getInstalment (){return this.instalment;}
+    getInstalment() { return this.instalment; }
+
+    getDateEnrollment() { return this.dateEnrollment; }
+
+    getCurrentDate() { return this.currentDate; }
+
+    //setters
+    setName(newName) { this.name = newName; }
+
+    setLastName(newLastName) { this.lastName = newLastName; }
+
+    setAge(newAge) { this.age = newAge; }
+
+    setHeight(newHeight) { this.height = newHeight; }
+
+    setWeight(newWeight) { this.weight = newWeight; }
+
+    setInstalment(newInstalment) { this.instalment = newInstalment; }
+ 
 }
+
+module.exports = User;
