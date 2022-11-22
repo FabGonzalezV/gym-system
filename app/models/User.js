cab01194ae91f0f40bf6afeacea63626694f2674
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2022-11-21 18:28:23 -0600

    user class

--- app/models/User.js
+++ app/models/User.js
@@ -1,1 +1,23 @@
-  
+
+class User {
+    constructor(name, lastName, age, height, weight, instalment) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.instalment = instalment;
+    }
+
+    getName() { return this.name }
+
+    getLastName() { return this.lastName; }
+
+    getAge(){return this.age;}
+
+    getHeight(){return this.height;}
+
+    getWeight(){return this.weight;}
+
+    getInstalment (){return this.instalment;}
+}
